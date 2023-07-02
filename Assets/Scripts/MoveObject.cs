abc398541e1822db1ac4b54671110c3a5445fb51
Brian How <bhow245@gmail.com>
2023-07-02 01:54:21 -0700

    Whales

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -10,9 +10,9 @@ public class MoveObject : MonoBehaviour
         var transform1 = transform;
         var position = transform1.position;
         transform.position = Vector3.MoveTowards(position,
-            new Vector3(-60, position.y, position.z), Speed * Time.deltaTime);
+            new Vector3(-70, position.y, position.z), Speed * Time.deltaTime);
         //transform.Translate(Vector3.left * (Speed * Time.deltaTime),Space.World);
-        if (transform.position.x < -50)
+        if (transform.position.x < -60)
         {
             Destroy(gameObject);
         }
